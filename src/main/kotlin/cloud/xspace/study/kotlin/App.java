20fd7cc5b35bcf02b8a85b61dc3df9b083e12ea1
bruce.wang <luckydog314@hotmail.com>
2019-01-07 14:30:22 +0800

    init my-kotlin project, with a hello world

---
+++ src/main/kotlin/cloud/xspace/study/kotlin/App.java
@@ -0,0 +1,13 @@
+package cloud.xspace.study.kotlin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Java!" );
+    }
+}
