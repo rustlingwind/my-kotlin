9171919def527b8e4f32b39a02463aecf72d21bb
bruce.wang <luckydog314@hotmail.com>
2019-01-08 18:19:56 +0800

    string template in Kotlin

---
+++ src/main/kotlin/fun/bruce/study/kotlin/App.kt
@@ -0,0 +1,28 @@
+/**
+ * This module is used to show the basic usage of string template in Kotlin
+ *
+ * @author Wang Liang
+ * @since 8th/Jan/2019
+ */
+fun main(args:Array<String>) {
+    printName1(args)
+    printName2(args)
+    printName3(args)
+}
+
+fun printName1(args: Array<String>) {
+    val name = getName(args)
+    println("Hello $name ! (1st)")
+}
+
+fun printName2(args: Array<String>) {
+    println("Hello ${getName(args)} ! (2nd)")
+}
+
+fun printName3(args: Array<String>) {
+    println("Hello ${if (args.isNotEmpty()) args[0] else "Kotlin"} ! (3rd)")
+}
+
+fun getName(args:Array<String>): String {
+    return if (args.isNotEmpty()) args[0] else "Kotlin"
+}
