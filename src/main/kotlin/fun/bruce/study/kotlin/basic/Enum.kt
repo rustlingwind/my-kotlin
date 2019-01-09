440192b656f35a19e38fe7ac30b1143456b1acd8
bruce.wang <luckydog314@hotmail.com>
2019-01-09 16:58:03 +0800

    update the Enum demo, add some functions with when expression

--- src/main/kotlin/fun/bruce/study/kotlin/basic/Enum.kt
+++ src/main/kotlin/fun/bruce/study/kotlin/basic/Enum.kt
@@ -1,23 +1,48 @@
 package `fun`.bruce.study.kotlin.basic
 
-enum class Color1 {
-    RED, GREEN, BLUE
-}
+import kotlin.Exception
 
-enum class Color2(val r:Int, val g:Int, val b:Int) {
-    RED(255, 0, 0), GREEN(0, 255, 0), BLUE(0, 0, 255);
+enum class Color(val r:Int, val g:Int, val b:Int) {
+    RED(255, 0, 0),
+    ORANGE(255,165,0),
+    YELLOW(255,255,0),
+    GREEN(0, 255, 0),
+    BLUE(0, 0, 255),
+    INDIGO(75,0,130),
+    VIOLET(238,130,238);
 
+    /**
+     * a simple function
+     */
     fun rgb() = (r * 256 + g) * 256 + b
-}
 
-fun main(args:Array<String>) {
-    println(Color1.BLUE)
-    println(Color1.BLUE.name)
-    println(Color1.BLUE.ordinal)
+    /**
+     * a function using the when expression with enum parameters
+     */
+    fun getWarmth() = when(this) {
+        RED, ORANGE, YELLOW -> "warm"
+        GREEN -> "neutral"
+        BLUE, INDIGO, VIOLET -> "cold"
+    }
 
-    println()
+    /**
+     * a function using the when expression with arbitrary type of parameters
+     */
+    fun mix(c: Color) = when (setOf(this, c)) {
+        setOf(RED, YELLOW) -> ORANGE
+        setOf(YELLOW, BLUE) -> GREEN
+        setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Dirty color")
+    }
 
-    println(Color2.BLUE.rgb())
-    println(Color2.RED.rgb())
-    println(Color2.GREEN.rgb())
+    /**
+     * a function using the when expression with no parameters
+     */
+    fun mixOptimized(c: Color) = when {
+        (this == RED && c == YELLOW) || (this == YELLOW && c == RED) -> ORANGE
+        (this == YELLOW && c == BLUE || this == BLUE && c == YELLOW) -> GREEN
+        this == BLUE && c == VIOLET || this == VIOLET && c == BLUE -> INDIGO
+        else -> throw Exception("Dirty color")
+    }
+
 }
--- src/test/kotlin/fun/bruce/study/kotlin/basic/EnumTest.kt
+++ src/test/kotlin/fun/bruce/study/kotlin/basic/EnumTest.kt
@@ -2,13 +2,30 @@ package `fun`.bruce.study.kotlin.basic
 
 
 fun main(args:Array<String>) {
-    println(Color1.BLUE)
-    println(Color1.BLUE.name)
-    println(Color1.BLUE.ordinal)
+    println("---- demonstrate enum objects:")
 
-    println()
+    println(Color.BLUE.name)
+    println(Color.BLUE.ordinal)
+    println(Color.BLUE)
 
-    println(Color2.BLUE.rgb())
-    println(Color2.RED.rgb())
-    println(Color2.GREEN.rgb())
+    println("---- demonstrate calling the functions of an enum object:")
+
+    println(Color.BLUE.rgb())
+    println(Color.RED.rgb())
+    println(Color.GREEN.rgb())
+
+    println("---- demonstrate calling the functions of an enum object:")
+
+    println(Color.RED.getWarmth())
+    println(Color.GREEN.getWarmth())
+    println(Color.BLUE.getWarmth())
+
+    println("---- demonstrate arbitrary type of parameters for when expression:")
+    println(Color.RED.mix(Color.YELLOW))
+
+    println("---- demonstrate a when expression with no parameters:")
+    println(Color.RED.mixOptimized(Color.YELLOW))
+    println(Color.YELLOW.mixOptimized(Color.BLUE))
+    println(Color.BLUE.mixOptimized(Color.VIOLET))
+
 }
