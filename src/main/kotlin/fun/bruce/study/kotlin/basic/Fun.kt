bb2dd6b9da2f1b72b9db58b8b9fa6e3df5a83730
bruce.wang <luckydog314@hotmail.com>
2019-01-11 14:01:06 +0800

    Fun with default parameters

---
+++ src/main/kotlin/fun/bruce/study/kotlin/basic/Fun.kt
@@ -0,0 +1,22 @@
+package `fun`.bruce.study.kotlin.basic
+
+fun <T> joinToString(
+        collection: List<T>,
+        separator: String = ",",
+        prefix: String = "[",
+        postfix: String = "]"
+): String {
+    val result = StringBuilder(prefix)
+
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+
+    result.append(postfix)
+    return result.toString()
+}
+
+fun main(args: Array<String>) {
+    println(joinToString(listOf(1,2,3)))
+}
